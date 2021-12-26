7e0d051ed2f396b884083f12dbef549b735be38c
Seth McCullough <seth@endevrs.dev>
2021-12-26 18:54:11 -0500

    Unknown and never types

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-console.log(combine(10, 20, "as-number"));
-console.log(combine("Seth", "Chen", "as-text"));
